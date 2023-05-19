5f5b8dbefd6f995a3b30ae161ceed41f8ac67b1b
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-05-19 07:32:18 +0600

    added custom css in category , and login page

--- src/Component/Home/Category/Category.jsx
+++ src/Component/Home/Category/Category.jsx
@@ -7,6 +7,7 @@ import "aos/dist/aos.css";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../AuthProvide/AuthProvider";
 import Swal from "sweetalert2";
+import './Category.css'
 
 const Category = () => {
   const [tabCategory, setTabCategory] = useState("Teddy");
